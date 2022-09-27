78d2dea788e1c31ac193e66c5174f13030992e08
mehdi-mtir <mehdi.mtir@gmail.com>
2022-09-27 14:09:13 +0100

    list comments avec modification de l'etat et gestion des events

--- src/App.js
+++ src/App.js
@@ -1,58 +1,84 @@
 import React from 'react';
 import Comment from './comment/Comment';
 import Card from './comment/Card';
-import Demo from './demo/Demo';
 
 //Pour définir un composant React :
 //- On peut utiliser une fonction (function based component)
 //- On peut utiliser une classe (class based component)
-let comments = [
-    {
-        nom: "Stevie Feliciano",
-        avatar: "https://semantic-ui.com/images/avatar/small/stevie.jpg",
-        dateComment: "12/09/2022",
-        evaluation: 5,
-        texte: "Hey guys, I hope this example comment is helping you read this documentation.",
-        etat : "en attente"
-    },
-    {
-        nom: "Elliot Fu",
-        avatar: "https://semantic-ui.com/images/avatar/small/elliot.jpg",
-        dateComment: "22/09/2022",
-        evaluation: 4,
-        texte: "Any comment!",
-        etat : "en attente"
-    },
-    {
-        nom: "Tom Lukic",
-        avatar: "https://semantic-ui.com/images/avatar/small/joe.jpg",
-        dateComment: "12/09/2022",
-        evaluation: 2,
-        texte: "Hello from React training.",
-        etat : "en attente"
+class App extends React.Component{
+    state = {
+        comments : [
+            {
+                id : 1,
+                nom: "Stevie Feliciano",
+                avatar: "https://semantic-ui.com/images/avatar/small/stevie.jpg",
+                dateComment: "12/09/2022",
+                evaluation: 5,
+                texte: "Hey guys, I hope this example comment is helping you read this documentation.",
+                etat : "en attente"
+            },
+            {
+                id : 2,
+                nom: "Elliot Fu",
+                avatar: "https://semantic-ui.com/images/avatar/small/elliot.jpg",
+                dateComment: "22/09/2022",
+                evaluation: 4,
+                texte: "Any comment!",
+                etat : "en attente"
+            },
+            {
+                id : 3,
+                nom: "Tom Lukic",
+                avatar: "https://semantic-ui.com/images/avatar/small/joe.jpg",
+                dateComment: "12/09/2022",
+                evaluation: 2,
+                texte: "Hello from React training.",
+                etat : "en attente"
+            }
+        ]
     }
-]
 
-const App = () => {
-    return <>
-        {
-            comments.map(
-                comment => {
-                    return (
-                    <Card key = {comment.nom} etat={comment.etat}>
-                        <Comment 
-                        avatar = {comment.avatar}
-                        nom = {comment.nom}
-                        dateComment = {comment.dateComment}
-                        evaluation = {comment.evaluation}
-                        texte = {comment.texte} />
-                    </Card>
-                    );
+    changeState = (idComment, newState)=>{
+        //console.log(idComment, newState);
+        const nouvelEtat = this.state.comments.map(
+            comment =>{
+                if(comment.id == idComment){
+                    return {...comment, etat : newState}
                 }
-            )
-        }
+                else{
+                    return comment
+                }
+            }
+        );
+        this.setState({comments : nouvelEtat});
+    }
 
-    </>
-}
+    render(){
+        return <>
+            {
+                this.state.comments.map(
+                    comment => {
+                        return (
+                        <Card 
+                            key = {comment.id} 
+                            etat={comment.etat} 
+                            changeHandler={this.changeState} 
+                            id={comment.id}>
+                                <Comment 
+                                avatar = {comment.avatar}
+                                nom = {comment.nom}
+                                dateComment = {comment.dateComment}
+                                evaluation = {comment.evaluation}
+                                texte = {comment.texte}
+                                id = {comment.id} />
+                        </Card>
+                        );
+                    }
+                )
+            }
+    
+        </>
+    }
+} 
 
 export default App;
--- src/comment/Card.js
+++ src/comment/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Card = (props)=>{
-    //console.log(props);
+    console.log(props);
     return <div className="ui cards">
     <div className="card">
       <div className="content">
@@ -10,8 +10,8 @@ const Card = (props)=>{
       </div>
       <div className="extra content">
         <div className="ui two buttons">
-          <div className="ui basic green button">Approve</div>
-          <div className="ui basic red button">Decline</div>
+          <div className="ui basic green button" onClick={()=>props.changeHandler(props.id, "Approuvé")}  >Approve</div>
+          <div className="ui basic red button" onClick={()=>props.changeHandler(props.id, "Décliné")}  >Decline</div>
         </div>
       </div>
     </div>
